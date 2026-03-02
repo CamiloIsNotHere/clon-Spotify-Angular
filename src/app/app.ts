9f127a3f9c6efb5d851bc7aec605557d8089c344
Camilo <camiloitsonline@gmail.com>
2026-03-02 16:00:16 -0500

    feat: add home page and routing
    - Implemented routing to the home page in app.routes.ts
    - Created Home component with basic template and styles
    - Updated app component to navigate to home page
    - Added global styles and imported Google font
    - Created unit tests for Home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('clon-Spotify-Angular');
+
+  constructor(private router: Router) {}
+
+  navegar() {
+    this.router.navigate(['/home']);
+  }
+
+  songs = [
+    {
+      title: 'Danza Kuduro',
+      artist: 'Don Omar',
+      image: './assets/images/DonOmar.png',
+      song: './assets/mp3/Danza Kuduro_spotdown.org.mp3',
+    },
+  ];
 }
